b15e177eb3965276874ea3d70afc76836e8efcb0
Fatna AbdulTaib <phatnaabdul55@gmail.com>
2023-08-18 16:03:22 +0300

    installed django restful framework ,created serializers file

--- backend/blogapp/models.py
+++ backend/blogapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Blog(models.Model):
+    blogTitle=models.CharField(max_length=150)
+    blogPost=models.TextField()
+    author=models.CharField(max_length=30)
+    date_published=models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return  self.blogTitle  
--- backend/blogproject/settings.py
+++ backend/blogproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'blog',
+        'USER':'postgres',
+        'PASSWORD':'phertnerh55',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
